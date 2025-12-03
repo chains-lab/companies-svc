834009376f99ea1f816ef3cad3c509342f883d0b
paranoideed <yehorushakov@gmail.com>
2025-12-03 02:23:39 +0200

    init

--- internal/domain/errx/common.go
+++
@@ -1,9 +0,0 @@
-package errx
-
-import (
-	"github.com/chains-lab/ape"
-)
-
-var ErrorInternal = ape.DeclareError("INTERNAL_ERROR")
-
-var ErrorNotEnoughRight = ape.DeclareError("ERROR_NOT_ENOUGH_RIGHT")
--- resources/enum_types.go
+++
@@ -1,8 +0,0 @@
-package resources
-
-const (
-	CompanyType      = "company"
-	CompanyBlockType = "company_block"
-	EmployeeType     = "employee"
-	InviteType       = "company_invite"
-)
